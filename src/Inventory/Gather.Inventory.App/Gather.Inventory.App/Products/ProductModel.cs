0f3986a2f88c07f7a0329c1678627ff815693a54
Malek Atwiz <malekatwiz@gmail.com>
2023-02-27 01:01:08 -0500

    catalogue page

---
+++ src/Inventory/Gather.Inventory.App/Gather.Inventory.App/Products/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Gather.Inventory.App.Products;
+public class ProductModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string Brand { get; set; }
+    public string Weight { get; set; }
+    public string Size { get; set; }
+    public string BarCode { get; set; }
+    public List<string> Images { get; set; }
+    public string DefaultImage { get; set; }
+}
---
+++ src/Inventory/Gather.Inventory.App/Gather.Inventory.App/Usings.cs
@@ -0,0 +1,1 @@
+﻿global using Gather.Inventory.App.Products;
